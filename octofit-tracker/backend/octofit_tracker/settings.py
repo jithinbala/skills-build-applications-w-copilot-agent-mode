d5907de8858cdab4e9941c47a8441276d08acdc8
Jithin Pattamthody <jithin18@hotmail.com>
2025-04-08 12:11:59 +0000

    Update views.py and settings.py with codespace URL and allowed hosts

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-b!c2+sykskd0npu02)!p(*qjg_cdn=qxxka)c%hs7t_+cb*fum
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'solid-goldfish-pjpp74wg69773rjpp-8000.app.github.dev']
 
 
 # Application definition
